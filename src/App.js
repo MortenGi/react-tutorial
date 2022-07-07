b923bcf1ecc2c2f4ed978087937af591e6383714
Giese, Morten (Mac) <mgiese@mpa-garching.mpg.de>
2022-07-07 12:23:09 +0200

    add state for Functional and Class-based comps

--- src/App.js
+++ src/App.js
@@ -2,17 +2,65 @@ import logo from "./logo.svg";
 import "./App.css";
 import {PropTypes} from "prop-types"
 import Info from "./Info.js";
+import {useState} from "react"
 
 function App() {
   return (
     <div className="App">
       <Info />
       <Info title ="This title was passed as a prop" />
-      <AddItem text="Joe" number={2} />
+      <ButtonState />
+      {/* <AddItem text="Joe" number={2} /> */}
     </div>
   );
 }
 
+//working with stats: functionals states
+function ButtonState() {
+  //make a hook: usestate. hook into react features
+  // const [variable, function] to make a state
+  // defines the functions for you!!
+  // with a hook we do not need to convert the functional component to a class!
+  const [title, setTitle] = useState("");
+  const [count, setCount] =  useState(0);
+
+  const updateTitleClicked = ()=>{
+    setTitle('We now have a title');
+
+  };
+  const updateCounterClicked = ()=>{
+    setCount(count + 1);
+    
+  };
+  return (
+      <div>
+
+      <Data title={title} count={count} />
+      <button onClick={updateTitleClicked}>Update Title</button>
+      <button onClick={updateCounterClicked}>Update Count</button>
+      </div>
+
+  );
+
+}
+
+//passing state down to other components
+function Data(props) {
+  return(
+    <div>
+    <p>Title: {props.title}</p>
+    <p>Counter: {props.count}</p>
+
+    </div>
+  )
+}
+
+
+export default App;
+
+
+
+/*
 //example of a functional based component
 function AddItem(props) {
   //Read-only
@@ -35,7 +83,6 @@ AddItem.defaultProps = {
 
 AddItem.propTypes = {
   number: PropTypes.number, 
-
+ // <AddItem text="Joe" number="hahahah" /> will give error
 }
-
-export default App;
+*/
--- src/Info.js
+++ src/Info.js
@@ -1,14 +1,36 @@
 import React from "react";
-import {PropTypes} from "prop-types";
+import { PropTypes } from "prop-types";
 
 //Example of a class based component
 
 class Info extends React.Component {
-    constructor(props){
-        super(props);
-        //just to get properties
-        //will be read only
-    }
+  constructor(props) {
+    super(props);
+    //just to get properties
+    //will be read only
+
+    //lets enable state for the class based comp
+    this.state = {
+      count: 0,
+      title: "Hello",
+    };
+  }
+  buttonPressed = () => {
+    this.setState({
+      count: this.state.count + 1,
+    });
+  };
+
+  render() {
+    return (
+    <div>
+      <p>Count: {this.state.count}</p>
+      <button onClick={this.buttonPressed}>Class based component: State update</button>
+    </div>
+)  
+};
+
+  /*
   render() {
     const title = this.props.title; 
     return (
@@ -21,12 +43,12 @@ class Info extends React.Component {
 }
 
 Info.defaultProps = {
-    title:"default"
-
-}
+  title: "default",
+};
 
 Info.propTypes = {
-    title: PropTypes.string, 
-   // <AddItem text="Joe" number="hahahah" /> will give error
-  }
+  title: PropTypes.string,
+  // <AddItem text="Joe" number="hahahah" /> will give error
+};
+*/
 export default Info;
