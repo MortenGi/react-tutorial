7ae6ef025c721d2ec29d7731d6d55ef2996dbe0a
Giese, Morten (Mac) <mgiese@mpa-garching.mpg.de>
2022-07-08 19:34:31 +0200

    add backend server

--- src/App.js
+++ src/App.js
@@ -23,7 +23,19 @@ function App() {
 
   const addItemToData = (item) => {
     let items = data["items"];
-    item.id = items.length;
+
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "Application/json" },
+      body: JSON.stringify(item),
+    };
+    fetch("http://localhost:3000/items", requestOptions)
+      .then((_res) => _res.json())
+      .then((data) => {
+        items.push(data);
+        setData({ items: items });
+      });
+
     items.push(item);
     setData({ items: items });
   };
